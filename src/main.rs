54802b128667d486f222fcc898dceb4c85184633
lpnh <paniguel.lpnh@gmail.com>
2025-10-14 13:15:01 -0300

    remove clippy allow attributes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-#![allow(clippy::cast_lossless)]
-#![allow(clippy::cast_possible_truncation)]
-#![allow(clippy::cast_precision_loss)]
-#![allow(clippy::cast_sign_loss)]
-
 mod canvas;
 use canvas::Canvas;
 mod registry;
